70212e6da1f227b01ba0c5b43515169acba5c00e
BazarovAndrey <andrey.bazarov75@gmail.com>
2021-11-14 04:42:37 +0700

    feat: add argument passing

--- Production Line/main.c
+++ Production Line/main.c
@@ -9,31 +9,37 @@
 sem_t detail_A, detail_B, detail_C, module;
 
 void *create_detail_A(void *arg) {
+    int *development_time_of_detail_A = (int *) arg;
+
     while (1) {
-        sleep(1);
+        sleep(*development_time_of_detail_A);
         sem_post(&detail_A);
         printf("detail A created\n");
     }
 }
 
 void *create_detail_B(void *arg) {
+    int *development_time_of_detail_B = (int *) arg;
+
     while (1) {
-        sleep(2);
+        sleep(*development_time_of_detail_B);
         sem_post(&detail_B);
         printf("detail B created\n");
     }
 }
 
 void *create_detail_C(void *arg) {
-    while(1) {
-        sleep(3);
+    int *development_time_of_detail_C = (int *) arg;
+
+    while (1) {
+        sleep(*development_time_of_detail_C);
         sem_post(&detail_C);
         printf("detail C created!\n");
     }
 }
 
 void *create_module(void *arg) {
-    while(1){
+    while (1) {
         sem_wait(&detail_A);
         sem_wait(&detail_B);
         sem_post(&module);
@@ -42,30 +48,59 @@ void *create_module(void *arg) {
 }
 
 void create_widget() {
-    while(1) {
+    int number_of_widgets = 0;
+
+    while (1) {
         sem_wait(&module);
         sem_wait(&detail_C);
-        printf("widget created!\n");
+        number_of_widgets++;
+        printf("widget created! number of invented widgets: %d\n", number_of_widgets);
     }
 }
 
 int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "not enough arguments\n");
+        exit(EXIT_FAILURE);
+    }
+
     pthread_t threads[NUMBER_OF_FACTORIES];
+
+    int *development_time_of_detail_A;
+    int *development_time_of_detail_B;
+    int *development_time_of_detail_C;
 
     sem_init(&detail_A, 0, 0);
     sem_init(&detail_B, 0, 0);
     sem_init(&detail_C, 0, 0);
     sem_init(&module, 0, 0);
 
-    if (pthread_create(threads, NULL, create_detail_A, NULL) != 0) {
+    if ((development_time_of_detail_A = (int *) malloc(sizeof(int))) == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    if ((development_time_of_detail_B = (int *) malloc(sizeof(int))) == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    if ((development_time_of_detail_C = (int *) malloc(sizeof(int))) == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    *development_time_of_detail_A = atoi(argv[1]);
+    *development_time_of_detail_B = atoi(argv[2]);
+    *development_time_of_detail_C = atoi(argv[3]);
+
+    if (pthread_create(threads, NULL, create_detail_A, (void *) development_time_of_detail_A) != 0) {
         perror("pthread create");
         exit(EXIT_FAILURE);
     }
-    if (pthread_create(threads + 1, NULL, create_detail_B, NULL) != 0) {
+    if (pthread_create(threads + 1, NULL, create_detail_B, (void *) development_time_of_detail_B) != 0) {
         perror("pthread create");
         exit(EXIT_FAILURE);
     }
-    if (pthread_create(threads + 2, NULL, create_detail_C, NULL) != 0) {
+    if (pthread_create(threads + 2, NULL, create_detail_C, (void *) development_time_of_detail_C) != 0) {
         perror("pthread create");
         exit(EXIT_FAILURE);
     }
@@ -80,6 +115,9 @@ int main(int argc, char *argv[]) {
     sem_destroy(&detail_B);
     sem_destroy(&detail_C);
     sem_destroy(&module);
+    free(development_time_of_detail_A);
+    free(development_time_of_detail_B);
+    free(development_time_of_detail_C);
 
     pthread_exit(NULL);
 }
